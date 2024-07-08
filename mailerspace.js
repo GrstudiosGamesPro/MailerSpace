e692a77eb5c8a671d4788568039090995b4674e8
Esteban Nova <85538374+GrstudiosGamesPro@users.noreply.github.com>
2024-07-07 21:15:45 -0500

    Add files via upload

---
+++ mailerspace.js
@@ -0,0 +1,51 @@
+function send_post(file_name, data, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        callback(null, xhr.responseText);
+      } else {
+        callback("Hubo un error en la solicitud.", null);
+      }
+    }
+  };
+
+  xhr.open("POST", file_name, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(data);
+}
+
+function send_post_get(file_name, data, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        callback(null, xhr.responseText);
+      } else {
+        callback("Hubo un error en la solicitud.", null);
+      }
+    }
+  };
+
+  xhr.open("GET", file_name, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.send(data);
+}
+
+function send_data_email(data) {
+  let url = "http://localhost/mailerspace/send.php";
+
+  let queryString = new URLSearchParams(data).toString();
+
+  function solution_callback(error, respuesta) {
+    if (error) {
+      console.error("Error: " + error);
+    } else {
+      console.log("Response: " + respuesta);
+    }
+  }
+
+  send_post(url, queryString, solution_callback);
+}
